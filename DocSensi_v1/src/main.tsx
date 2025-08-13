40dfece0cd2581ca99475400427887d73321f06d
Tushar Sharma <tushar.sharma78000@gmail.com>
2025-08-12 23:08:47 -0400

    Additional upadtes for deployment

--- DocSensi_v1/src/main.tsx
+++ DocSensi_v1/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
+import { pdfjs } from 'react-pdf';
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker?url';
+pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import './pdf-viewer-styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>,
 );
